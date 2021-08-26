7a7d15b9ed3d4a41be4ff788cb6ec3fe4674a42f
Houston Bass <houstonbass96@gmail.com>
2021-08-26 09:25:44 -0400

    join button switches scene and joins server
    added global to pass data between scenes

--- Lobby.gd
+++ Lobby.gd
@@ -1,14 +1,24 @@
-extends Control
+extends Node
 
-func _ready():
-	pass # Replace with function body.
+var match_resource = load("res://Match.tscn")
 
+var text : String = ''
+
+func _on_LineEdit_text_changed(new_text):
+	text = new_text
 
 func _on_Join_pressed():
-	print("connect to server")
-	# call server 
+	if textIsValid():
+		var root = get_tree().get_root()
+		var current_scene = root.get_child(root.get_child_count() - 1)
+		root.remove_child(current_scene)
+		current_scene.call_deferred("free")
+		
+		Global.set_player_name(text)
+		
+		var Match = match_resource.instance()
+		root.add_child(Match)
+	pass
 	
-	# load scene match
-	
-	
-	pass # Replace with function body.
+func textIsValid():
+	return text.length() >= 3
--- Match.gd
+++ Match.gd
@@ -1,7 +1,25 @@
 extends Control
 
-const Player = preload("res://Player.tscn").instance()
+# will need to configure this somehow, either with ip of actual server or an ip setting method
+const ip = "127.0.0.1"
+const port = 5000
 
-func player_join():
-	add_child(Player())
-	pass
+var network = NetworkedMultiplayerENet.new()
+
+func _ready():
+	ConnectToServer()
+
+func ConnectToServer():
+	print('Player name: '+ Global.get_player_name())
+	network.create_client(ip, port)
+	get_tree().set_network_peer(network)
+	
+	get_tree().connect("connection_failed", self, "_on_connection_failed")
+	get_tree().connect("connected_to_server", self, "_on_connection_succeeded")
+
+
+func _on_connection_failed():
+	print('Failed to connect to server :(')
+	
+func _on_connection_succeeded():
+	print('Connected to server!')
